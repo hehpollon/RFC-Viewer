44bc1ba36f4e711684f7f32f44c352b95fde8605
hehpollon <hehpollon@gmail.com>
2018-04-25 17:48:56 +0900

    modify applicationId

--- app/src/androidTest/java/com/hehpollon/rfc/viewer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/hehpollon/rfc/viewer/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.signkorea.myapplication", appContext.getPackageName());
+        assertEquals("com.hehpollon.rfc.viewer", appContext.getPackageName());
     }
 }
